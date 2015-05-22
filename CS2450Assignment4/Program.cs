858e1e8daa2119d57f3634035b796716c46343c5
Sunny Burtt <reiwen1@yahoo.com>
2015-05-21 19:13:01 -0600

    Commit #1

---
+++ CS2450Assignment4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS2450Assignment4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
